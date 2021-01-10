fc17742ca57a2431ae018dd93d431e7b66031551
Yevhen Bakhov <ostneel@gmail.com>
2021-01-10 19:59:11 +0200

    All morphological transformations applied to image

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+fps = 60                                            # Frames per second of incoming video
 
+incoming_video = cv2.VideoCapture('some_video.mp4')
+subtractor = cv2.bgsegm.createBackgroundSubtractorMOG()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+while True:
+    self, original_video = incoming_video.read()    # Reading from input video stream
+    time = float(1 / fps)
+    sleep(time)                                     # Waiting for the next frame
 
+    # Turns original colors into grayscale
+    grayscale = cv2.cvtColor(original_video, cv2.COLOR_BGR2GRAY)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Blur for smoothing boundaries
+    blurred = cv2.GaussianBlur(grayscale, (3, 3), 5)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Separates background and creates foreground mask for easier detection
+    subtracted = subtractor.apply(blurred)
+
+    # Provides image dilation
+    dilated = cv2.dilate(subtracted, np.ones((5, 5)))
+
+    # Get kernel with ellipse form and size 5x5
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
